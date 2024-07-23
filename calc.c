56b0c4f26211aea1f72591b9641f3a27fc0a865f
soukouki <soukouki0@yahoo.co.jp>
2024-07-23 12:06:24 +0900

    ステップ10

--- calc.c
+++ calc.c
@@ -11,6 +11,20 @@ int integer(void) {
   return number;
 }
 
+int mul_div(void) {
+  int result = integer();
+  while(buf[idx] == '*' || buf[idx] == '/') {
+    if(buf[idx] == '*') {
+      idx++;
+      result *= integer();
+    } else if(buf[idx] == '/') {
+      idx++;
+      result /= integer();
+    }
+  }
+  return result;
+}
+
 int add_sub(void) {
   int result = integer();
   while(buf[idx] == '+' || buf[idx] == '-') {
@@ -27,6 +41,6 @@ int add_sub(void) {
 
 int main(void) {
   scanf("%255s", buf);
-  int result = add_sub();
+  int result = mul_div();
   printf("%d\n", result);
 }
