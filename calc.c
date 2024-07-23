8d4c63b4b87564ce665abf8c8d6558cf6ea18922
soukouki <soukouki0@yahoo.co.jp>
2024-07-23 11:57:34 +0900

    ステップ4

--- calc.c
+++ calc.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
-  char buf[256];
-  scanf("%255s", buf);
+char buf[256];
+int idx = 0;
+
+int integer(void) {
   int number = 0;
-  for(int i = 0; '0' <= buf[i] && buf[i] <= '9'; i++) {
-    number = number * 10 + buf[i] - '0';
+  for(; '0' <= buf[idx] && buf[idx] <= '9'; idx++) {
+    number = number * 10 + buf[idx] - '0';
   }
+  return number;
+}
+
+int main(void) {
+  scanf("%255s", buf);
+  int number = integer();
   printf("%d\n", number);
 }
