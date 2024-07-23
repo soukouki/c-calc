11e96354e99064b8fd49de65fba6d72f589e3d28
soukouki <soukouki0@yahoo.co.jp>
2024-07-23 11:55:14 +0900

    ステップ3

--- calc.c
+++ calc.c
@@ -3,6 +3,9 @@
 int main(void) {
   char buf[256];
   scanf("%255s", buf);
-  int number = buf[0] - '0';
+  int number = 0;
+  for(int i = 0; '0' <= buf[i] && buf[i] <= '9'; i++) {
+    number = number * 10 + buf[i] - '0';
+  }
   printf("%d\n", number);
 }
